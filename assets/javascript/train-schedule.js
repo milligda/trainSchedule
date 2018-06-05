c07b3e41f35c2d53c5f16da0a796c9c8880e9b70
DMilligan <dmilligan@springlocker.com>
2018-06-05 11:15:38 -0400

    adding modals

--- assets/javascript/train-schedule.js
+++ assets/javascript/train-schedule.js
@@ -22,6 +22,8 @@ $(document).ready(function() {
     var trainFrequency = "";
     var trainID = 0;
     var allStops = [];
+    var nextArrival;
+    var minutesAway;
 
     // connect to the database on the page load and count the number of items in the database
     database.ref().on("value", function(snap) {
@@ -65,24 +67,53 @@ $(document).ready(function() {
 
         console.log(childSnapshot.val());
 
-        var snapFrequency = childSnapshot.val().frequency; // string
-        snapFrequency = parseInt(snapFrequency);  // integer 15
+        var snapFrequency = childSnapshot.val().frequency;
+        snapFrequency = parseInt(snapFrequency);
 
-
-
-        var snapStartTime = childSnapshot.val().startTime; // string in 08:10 format
+        var snapStartTime = childSnapshot.val().startTime;
 
         var convertedStart = moment(snapStartTime, 'HH:mm'); 
-        console.log(convertedStart);
 
+        var timeDifference = moment().diff(moment(convertedStart), "minutes");
+
+        var timeRemainder = timeDifference % snapFrequency;
+
+        if (moment().diff(moment(convertedStart), "minutes") < 0) {
+            
+            minutesAway = Math.abs(timeDifference);
+            nextArrival = convertedStart;
+        
+        } else {
+
+            minutesAway = snapFrequency - timeRemainder;
+            nextArrival = moment().add(minutesAway, "minutes");
+        }
+
+        nextArrival = moment(nextArrival).format("h:mma");
+
+        var stopsArray = childSnapshot.val().stops.split(', ');
+
+        populateTable(childSnapshot.val());
+
+        $("#destination-menu").empty();
+
+        populateStopsDropdown(childSnapshot.val(), stopsArray);
+
+        populateTrainModal(childSnapshot.val(), nextArrival, minutesAway, stopsArray);
+    });
+
+    function populateTable(train) {
 
         var newRow = $('<tr>');
-        var nameTD = $('<td>' + childSnapshot.val().Name + '</td>');
-        var finalStopTD = $('<td>' + childSnapshot.val().destination + '</td>');
-        var frequencyTD = $('<td>' + childSnapshot.val().frequency + '</td>');
-        var nextArrivalTD = $('<td>' +  + '</td>');
-        var minutesAwayTD = $('<td>' + '</td>');
+        var nameTD = $('<td>');
+        var nameModalLink = $('<a class="train-name" data-toggle="modal">' + train.Name + '</a>');
+        nameModalLink.attr('data-target', '#trainSchedule' + train.id);
+        var finalStopTD = $('<td>' + train.destination + '</td>');
+        var frequencyTD = $('<td>' + train.frequency + '</td>');
+        var nextArrivalTD = $('<td>' + nextArrival + '</td>');
+        var minutesAwayTD = $('<td>' + minutesAway + '</td>');
 
+        nameModalLink.appendTo(nameTD);
         nameTD.appendTo(newRow);
         finalStopTD.appendTo(newRow);
         frequencyTD.appendTo(newRow);
@@ -90,29 +121,66 @@ $(document).ready(function() {
         minutesAwayTD.appendTo(newRow);
 
         newRow.appendTo("#train-schedule");
+    }
 
-        var stopsArray = childSnapshot.val().stops.split(', ');
+    function populateStopsDropdown(train, stops) {
 
-        for (var i = 0; i < stopsArray.length; i++) {
-            allStops.push(stopsArray[i]);
+        for (var i = 0; i < stops.length; i++) {
+            allStops.push(stops[i]);
         }
 
         allStops.sort();
 
-        $("#destination-menu").empty();
-
         for (var i = 0; i < allStops.length; i++) {
             var trainStop = $("<button>");
             trainStop.addClass('dropdown-item');
-            trainStop.attr('train-id', childSnapshot.val().id);
+            trainStop.attr('data-toggle', 'modal');
+            trainStop.attr('data-target', '#trainSchedule' + train.id);
             trainStop.text(allStops[i]);
             trainStop.appendTo("#destination-menu");
         }
-    });
+    }
 
+    function populateTrainModal(train, nextArrival, minutesAway, stops) {
+
+        var trainModal = $('<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true" id="trainSchedule' + train.id + '">');
+        var modalDialog = $('<div class="modal-dialog modal-dialog-centered" role="document">');
+        var modalContent = $('<div class="modal-content">');
+        var modalHeader = $('<div class="modal-header">');
+        var modalTitle = $('<h5 class="modal-title">' + train.Name + '</h5>');
+        var modalClose = $('<button type="button" class="close" data-dismiss="modal" aria-label="Close">');
+        var modalCloseButton = $('<span aria-hidden="true">');
+        modalCloseButton.text("x");
+        var modalBody = $('<div class="modal-body">');
+
+        var modalNextTrain = $('<p class="next-train-time">');
+        modalNextTrain.text('Next Train: ' + nextArrival);
+        modalNextTrain.appendTo(modalBody);
+
+        var modalArrivalTime = $('<p class="next-train-minutes">');
+        modalArrivalTime.text('arriving in ' + minutesAway + ' minutes');
+        modalArrivalTime.appendTo(modalBody);
+
+        var modalStopsHeader = $('<p class="train-stops">Stops:</p>');
+        modalStopsHeader.appendTo(modalBody);
         
+        for (var i = 0; i < stops.length; i++) {
+            var modalStop = $('<p class="train-stop">');
+            modalStop.text(stops[i]);
+            modalStop.appendTo(modalBody);
+        }
 
+        modalCloseButton.appendTo(modalClose);
+        modalTitle.appendTo(modalHeader);
+        modalClose.appendTo(modalHeader);
+        modalHeader.appendTo(modalContent);
+        modalBody.appendTo(modalContent);
+        modalContent.appendTo(modalDialog);
+        modalDialog.appendTo(trainModal);
+        trainModal.appendTo("#modal-container");
+    }
 
-
-
+    $("#add-train-button").on("click", function() {
+        $("#add-train-container").slideToggle("slow");
+    });
 });
